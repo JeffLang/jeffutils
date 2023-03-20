df5d3af74a770b7084d3a8f00bc97144219bb23a
jeff lang <langjinjie@tenacity.com.cn>
2023-03-20 16:30:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+### 安装
+
+```
+npm i jeffutils
+```
+
+
+### 导入
+
+```js
+import jeffutils from 'jeffutils'
+```
+
+
+### 包的使用
+
+    xxxxx
+       xxxx
+       xxxx
+
+### 开源协议
+
+   ISC
---
+++ src/common_utils.js
@@ -0,0 +1,21 @@
+export function deepClone(obj, hash = new WeakMap()) {
+  if (obj === null) return obj; // 如果是null或者undefined我就不进行拷贝操作
+  if (obj instanceof Date) return new Date(obj)
+  if (obj instanceof RegExp) return new RegExp(obj)
+  // 可能是对象或者普通的值  如果是函数的话是不需要深拷贝
+  if (typeof obj !== "object") return obj;
+  // 是对象的话就要进行深拷贝
+  // 防止对象无限嵌套自身的情况
+  const hashObj = hash.get(obj)
+  if (hashObj) return hashObj
+  let cloneObj = new obj.constructor()
+  // 找到的是所属类原型上的constructor,而原型上的 constructor指向的是当前类本身
+  hash.set(obj, cloneObj) // 此时是一个空对象 {}
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      // 实现一个递归拷贝
+      cloneObj[key] = deepClone(obj[key], hash)
+    }
+  }
+  return cloneObj
+}
